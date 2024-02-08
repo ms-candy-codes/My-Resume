548dc8bdb6f9d8951c9ab2a61c23a334168186f1
Jordan Candy <Jordan.candy07@outlook.com>
2024-02-08 10:45:33 +0200

    Create site

---
+++ index.js
@@ -0,0 +1,17 @@
+// let cardText = document.getElementsByClassName("cardText");
+
+// function addListeners(index) {
+//     cardText[index].addEventListener("mouseenter", () => {
+//         cardText[index].classList.add("displays");
+//         cardText[index].classList.remove("cardTexts");
+//     });
+
+//     cardText[index].addEventListener("mouseleave", () => {
+//         cardText[index].classList.remove("displays");
+//         cardText[index].classList.add("cardTexts");
+//     });
+// }
+
+// for (let i = 0; i < cardText.length; i++) {
+//     addListeners(i);
+// }
